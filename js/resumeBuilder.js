361ef1001b208254eb83c99eebe914c53dc933cc
Josue23 <josuerodrigues@gmail.com>
2017-01-31 20:23:50 -0200

    .append

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,3 +2,7 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 $("#main").append(["Josué"]); /* add a string Josué ao id main */
+
+var awesomeThoughts = 'I am Josué and I am AWESOME!';
+$("#main").append( '<br />' );
+$("#main").append(awesomeThoughts);
