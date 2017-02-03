73d498b93d0668bf6567d14b47aca34532515e18
Josue23 <josuerodrigues@gmail.com>
2017-02-03 13:09:03 -0200

    js/resumeBuilder.js string replace

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,7 +2,33 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 $("#main").append(["Josué"]); /* add a string Josué ao id main */
+$("#main").append( '<br />' );
 
-var awesomeThoughts = 'I am Josué and I am AWESOME!';
+var name = 'Josué';
+var awesomeThoughts = 'I am ' + name  + ' and I am AWESOME!';
+$("#main").append(awesomeThoughts);
+$("#main").append( '<br />' );
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+$("#main").append(funThoughts)
+$("#main").append( '<br />' );
+
+
+
+
+
 $("#main").append( '<br />' );
 $("#main").append(awesomeThoughts);
+$("#main").append( '<br />' );
+
+var email = 'josuerodrigues@gmail.com';
+$(main).append(email)
+console.log(email);
+$("#main").append( '<br />' );
+
+
+var newEmail = email.replace("gmail", "yahoo");
+$(main).append(newEmail)
+console.log(newEmail);
+$("#main").append( '<br />' );
+
